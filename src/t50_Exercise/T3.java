215ccf8ce0312362b4fc12eb3296a0c11391aa62
maon-maon <green@DESKTOP-UPG10EE>
2024-09-04 12:36:53 +0900

    20240904_1236_T3.java update

--- src/t50_Exercise/T3.java
+++ src/t50_Exercise/T3.java
@@ -2,6 +2,7 @@ package t50_Exercise;
 
 public class T3 {
 	public static void main(String[] args) {
+		System.out.println("1~10까지 출력");
 		for(int i=0; i<=10; i++) {
 			System.out.println("i : "+i);
 		}
